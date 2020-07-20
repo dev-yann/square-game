d606d205814973f4ce6db51d9e37d03c1479ff79
Yann <yanndumas54230@gmail.com>
2020-07-20 15:55:38 +0200

    square game

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+
+        <Square v-for="(square, index) in squares" :key="index" :square="square" :index="index" v-on:divide="divide"/>
+
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Square from './components/Square'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            Square
+        },
+        data() {
+            return {
+                squares: [
+                    {
+                        width: 300,
+                        height: 300,
+                        top: this.getRandom(0, window.innerHeight - 50),
+                        left: this.getRandom(0, window.innerWidth - 50),
+                        color: 'blue'
+                    }
+                ],
+                screen: {
+                    maxWidth: window.innerWidth - 50,
+                    maxHeight: window.innerHeight - 50
+                },
+                colors: ['blue', 'red', 'yellow', 'grey', 'black']
+            }
+        },
+        methods: {
+
+            getRandom(min, max) {
+                min = Math.ceil(min);
+                max = Math.floor(max);
+                return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+            },
+
+            divide: function (index) {
+
+                let width = this.squares[index].width;
+                // this.squares.splice(index, 1);
+                this.squares.splice(index, 1, ...this.generateSquare(width));
+
+                console.log(this.squares);
+            },
+
+            generateSquare: function (preWidth) {
+                let squareArray = [];
+
+                console.log(this.colors.length);
+
+                for (let i = 0; i <= 3; i++) {
+                    squareArray.push({
+                        width: preWidth / 4,
+                        height: preWidth / 4,
+                        top: this.getRandom(0, this.screen.maxHeight),
+                        left: this.getRandom(0, this.screen.maxWidth),
+                        color: this.colors[this.getRandom(0, this.colors.length)]
+                    })
+                }
+
+                console.log(squareArray);
+                return squareArray;
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        height: 100vh;
+        width: 100vw;
+        position: relative;
+    }
 </style>
---
+++ src/components/Square.vue
@@ -0,0 +1,51 @@
+<template>
+    <transition name="slide-enter">
+        <div class="square" v-if="show" v-bind:style="{ top: square.top + 'px', left: square.left + 'px', height: square.height + 'px', width: square.width + 'px', backgroundColor: square.color}"
+             @click.prevent="divide"></div>
+    </transition>
+</template>
+
+<script>
+    export default {
+        name: "Square",
+        props: ['square', 'index'],
+        data() {
+            return {
+                show: false
+            }
+        },
+        mounted() {
+
+            this.show = true
+
+        },
+
+        methods: {
+
+            divide: function () {
+                this.$emit('divide', this.index);
+            },
+
+        }
+    }
+</script>
+
+<style scoped>
+    .square {
+        position: absolute;
+        background-color: red;
+    }
+
+    .slide-enter-enter-active {
+        transition: all .3s ease;
+    }
+
+    .slide-enter-leave-active {
+        transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    }
+
+    .slide-enter-enter, .slide-fade-leave-to {
+        transform: translateY(-150px);
+        opacity: 0;
+    }
+</style>
